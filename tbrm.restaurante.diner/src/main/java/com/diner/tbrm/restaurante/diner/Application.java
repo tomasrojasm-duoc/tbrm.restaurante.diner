565afd591405911ba49cd44e327f09b379e664de
tomasrojasm-duoc <tomas.rojasm54@gmail.com>
2026-05-07 20:45:19 -0400

    Creación Microservicio Diner

---
+++ tbrm.restaurante.diner/src/main/java/com/diner/tbrm/restaurante/diner/Application.java
@@ -0,0 +1,13 @@
+package com.diner.tbrm.restaurante.diner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
